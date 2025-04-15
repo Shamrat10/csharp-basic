a366b9761d181ac865fe1a55b1fc9b2317bdf81c
Tanvir Ahmed Khan <ta9vir@gmail.com>
2025-04-16 00:51:34 +1000

    operators example

--- CsharpBasicsCode/Program.cs
+++ CsharpBasicsCode/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace CsharpBasicsCode
+﻿using CsharpBasicsCode.operators;
+
+namespace CsharpBasicsCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // Learning Operators 
+            OperatorsExample operatorsExample = new OperatorsExample();
+
+
+            Console.WriteLine("Addition: " + operatorsExample.Add());
+            Console.WriteLine("Sub: " + operatorsExample.Subtract());
+            Console.WriteLine("Multiply: " + operatorsExample.Multiply());
+            Console.WriteLine("Division: " + operatorsExample.Divide());
+            
         }
     }
 }
---
+++ CsharpBasicsCode/operators/OperatorsExample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasicsCode.operators
+{
+    public class OperatorsExample
+    {
+        //constructor
+        public int a;
+        public int b;
+
+        public int c;
+        public int d;
+
+
+        //Constructor
+        public OperatorsExample()
+        {
+            a = 30;
+            b = 20;
+        }
+
+
+
+        //Functions
+
+        //Addition
+        public int Add()
+        {
+            var result = a + b;
+            return result;
+        }
+
+        //subtraction
+        public int Subtract()
+        {
+            var result = a - b;
+            return result;
+        }
+
+        //Multiplication
+        public int Multiply()
+        {
+            var result = a * b;
+            return result;
+        }
+
+
+        //Division
+        public float Divide()
+        {
+            float result = (float)a / b; // Cast one operand
+            return result;
+        }
+
+    }
+}
